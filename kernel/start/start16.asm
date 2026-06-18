b66e169e7432d1214998b54fcf78ba287d66807b
iwi0ii1 <tangyenrui@gmail.com>
2026-06-18 19:30:56 +0800

    My first BIOS code in kernel/start/start16.asm... im still a beginner, and somehow, im astonished that they used the segments (es, gs, etc.) as parameters for interrupts.
    Fun fact, this is exactly what programs do in the 80s for IBM PC.

--- kernel/start/start16.asm
+++ kernel/start/start16.asm
@@ -2,17 +2,32 @@ bits 16
 global _start16
 extern _start64
 
-section .text
+section .text._start16
 _start16:
     ; Set VGA Graphics Mode 0x13
     mov ax, 0x13
     int 0x10
 
-    mov ah, 0x0E
-    mov al, 'A'
+    ; Set ES to the VGA segment
+    mov ax, 0xA000
+    mov es, ax
+    
+    ; Clear DI to start at the top-left of the screen
+    xor di, di
 
-    int 0x10 ; BIOS Interrupts for video things
+    mov al, 0x52
+    
+    ; Set CX to 64,000 (total pixels)
+    mov cx, 64000
+    
+    rep stosb ; Fill memory automatically!
 
 .hang:
     hlt
-    jmp .hang
+    jmp short .hang
+
+; Pad 512 bytes since BIOS requires exactly 512 bytes (u can exceed, tho will be ignored afterward)
+times 510-($-$$) db 0 
+
+; The "Magic Number" that the BIOS looks for to confirm it's bootable
+dw 0xaa55
