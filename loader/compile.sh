b72f91c3a6993aba3ffd11d327aec0ffdd5cf451
Idk-What-Is-0x7F-Is-It-127 <tangyenrui@gmail.com>
2026-07-06 22:19:46 +0800

    HWD is now structured by northbridges and southbridges and integrated and core. Chipsets yeah.

--- loader/compile.sh
+++ loader/compile.sh
@@ -12,6 +12,26 @@ if [[ "$1" == "bios" ]]; then
     mapfile -t SOURCES < <(find . -type f \( -name "*.asm" -o -name "*.s" \))
     
     printf "\nCompiling 16-bit loader.\n"
+    
+    # for file in "${SOURCES[@]}"; do
+        # file="${file#./}"
+        # newpath="$build_loc/$(echo "$file" | sed 's/\//_/g' | sed 's/\.[^.]*$/.bin/')"
+
+        # case "$file" in
+            # *.asm)
+                # printf "[+] Assembling: $file -> $newpath\n"
+                # nasm \
+                    # -f bin \
+                    # -I . \
+                    # "${file#./}" -o "$newpath" \
+                # ;;
+        
+            # *)
+                # printf "[-] Unknown file type: $file"
+                # exit 1
+                # ;;
+        # esac
+    # done
 
     printf "[+] Assembling bios/ls_main.asm -> $build_loc/loader.bin\n"
     nasm -f bin -I . "bios/ls_main.asm" -o "$build_loc/loader.bin"
